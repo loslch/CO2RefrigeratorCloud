1bb65834a66de1b8a2847de4cbe39a71097848db
Luna <cjaey77@gmail.com>
2014-06-08 15:26:42 +0900

    재용 첫 커밋

--- CO2RefrigeratorCloud/urls.py
+++ CO2RefrigeratorCloud/urls.py
@@ -5,6 +5,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', 'cloud.views.home'),
+    url(r'^cj/$', 'cloud.views.cj'),
 
     url(r'^admin/', include(admin.site.urls)),
 )
--- cloud/views.py
+++ cloud/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
 from django.shortcuts import render_to_response
+from django.http import HttpResponse
 
 def home(request):
     return render_to_response('home.html')
+
+def cj(request):
+    return HttpResponse('hello world cj')
