2d174f495174d0c79c554a1858596665af26b111
ChangHun Lee <loslch@gmail.com>
2014-06-13 18:01:48 +0900

    대시보드 기능 구현

--- cloud/templatetags/device_tag.py
+++ cloud/templatetags/device_tag.py
@@ -1,5 +1,6 @@
 from django import template
-from cloud.models import Device
+from django.db.models import Avg
+from cloud.models import Device, Status
 register = template.Library()
 
 
@@ -16,3 +17,50 @@ def is_display_devices_menu(u):
 def get_my_devices(u):
     devices = Device.objects.filter(user=u)
     return devices
+
+
+@register.filter
+def get_my_devices_and_status(u):
+    my_devices = Device.objects.filter(user=u)
+    device_and_status = my_devices.select_related('status')
+    return device_and_status
+
+
+@register.filter
+def get_avg_co2(u):
+    my_devices = Device.objects.filter(user=u)
+    device_and_status = my_devices.select_related('status')
+    sum = 0
+    for item in device_and_status:
+        sum += item.status.co2
+    return (sum/device_and_status.__len__())
+
+
+@register.filter
+def get_avg_o2(u):
+    my_devices = Device.objects.filter(user=u)
+    device_and_status = my_devices.select_related('status')
+    sum = 0
+    for item in device_and_status:
+        sum += item.status.o2
+    return (sum/device_and_status.__len__())
+
+
+@register.filter
+def get_avg_temperature(u):
+    my_devices = Device.objects.filter(user=u)
+    device_and_status = my_devices.select_related('status')
+    sum = 0
+    for item in device_and_status:
+        sum += item.status.temperature
+    return (sum/device_and_status.__len__())
+
+
+@register.filter
+def get_avg_humidity(u):
+    my_devices = Device.objects.filter(user=u)
+    device_and_status = my_devices.select_related('status')
+    sum = 0
+    for item in device_and_status:
+        sum += item.status.humidity
+    return (sum/device_and_status.__len__())
